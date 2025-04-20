5c8394c8e0a0d26efb312eb7f2896373528bc93f
ConstanzaOyarzo <coyarzod0@gmail.com>
2025-04-20 14:15:22 -0400

    api

--- main.py
+++ main.py
@@ -2,21 +2,72 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from Linked_Queue import LinkedQueue
 from Exceptions import OwnEmpty
+from models import Base, EstadoVuelo, Vuelo
+from typing import Optional, List
 
 app = FastAPI()
 lista_vuelos = LinkedQueue() # Instancia de Lista Vuelos
-# stack = ArrayStack()  # Instancia global
 
-class VueloBase(BaseModel):
+class VueloCreate(BaseModel):
     codigo: str
+    estado: Optional[EstadoVuelo] = EstadoVuelo.programado
+    origen: str
     destino: str
     aerolinea: str
 
-class VueloCreate(VueloBase):
-    pass
-
-class VueloOut(VueloBase):
+class VueloOut(VueloCreate):
     id: int
 
     class Config:
         orm_mode = True
+
+""" POST /vuelos """
+@app.post("/vuelos", response_model=VueloOut)
+def añadir_vuelo(vuelo: VueloCreate):
+    if vuelo.estado == "emergencia":
+        lista_vuelos.insertar_al_frente(vuelo)
+    else:
+        lista_vuelos.enqueue(vuelo)
+    return vuelo
+
+""" GET /vuelos/total """
+@app.get("/vuelos/total")
+def total_vuelos():
+    return {"total": lista_vuelos.size}
+
+""" GET /vuelos/próximo """
+@app.get("/vuelos/proximo", response_model=VueloOut)
+def proximo_vuelo():
+    if lista_vuelos.is_empty():
+        raise HTTPException(status_code=404, detail="No hay vuelos en la cola")
+    return lista_vuelos.first()
+
+""" GET /vuelos/ultimo """
+@app.get("/vuelos/ultimo", response_model=VueloOut)
+def ultimo_vuelo():
+    if lista_vuelos.is_empty():
+        raise HTTPException(status_code=404, detail="No hay vuelos en la cola")
+    return lista_vuelos.last()
+
+""" POST /vuelos/insertar """
+@app.post("/vuelos/insertar", response_model=VueloOut)
+def insertar_vuelo(vuelo: VueloCreate, posicion: int):
+    lista_vuelos.insertar_en_posicion(vuelo, posicion)
+    return vuelo
+
+""" DELETE /vuelos/extraer """
+@app.delete("/vuelos/extraer")
+def extraer_vuelo(posicion: int):
+    vuelo_removido = lista_vuelos.extraer_de_posicion(posicion)
+    return {"mensaje": "Vuelo removido", "vuelo": vuelo_removido}
+
+""" GET /vuelos/lista """
+@app.get("/vuelos/lista", response_model=List[VueloOut])
+def listar_vuelos():
+    return lista_vuelos.to_list()  # método que devuelva los vuelos como lista
+
+""" PATCH /vuelos/reordenar """
+@app.patch("/vuelos/reordenar")
+def reordenar_manual(nueva_lista: List[VueloCreate]):
+    lista_vuelos.reordenar(nueva_lista)  # método que reconstruye la cola
+    return {"mensaje": "Cola reordenada"}
